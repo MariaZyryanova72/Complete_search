db2cb95f4c0f0ce2b7a0632c5b6f771338302c61
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-07 17:43:00 +0500

    Программа поика в StaticMaps ВЕРСИЯ №1

---
+++ pos.py
@@ -0,0 +1,11 @@
+def object_pos(toponym_coodrinates_1, toponym_coodrinates_2):
+    x_1, y_1 = toponym_coodrinates_1.split(" ")
+    x_2, y_2 = toponym_coodrinates_2.split(" ")
+    x_1, y_1 = float(x_1), float(y_1)
+    x_2, y_2 = float(x_2), float(y_2)
+
+    toponym_longitude, toponym_lattitude = str(((x_2 - x_1) / 2) + x_1), str(((y_2 - y_1) / 2) + y_1)
+
+    delta_x = str(float(toponym_longitude) - x_1)
+    delta_y = str(float(toponym_lattitude) - y_1)
+    return toponym_longitude, toponym_lattitude, delta_x, delta_y
