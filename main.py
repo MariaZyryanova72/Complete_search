ec5410a69ab079db5da24614d6dfbbedbea3f676
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-08 10:35:37 +0500

    Программа поика в StaticMaps поиск ближайшей аптеки относительно введенного адреса

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
-import sys
 from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-
+import sys
 import requests
 from PIL import Image
-from Complete_search.pos import object_pos
+from pos import object_pos
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
-toponym_to_find = " ".join(sys.argv[1:])
-
+search_api_server = "https://search-maps.yandex.ru/v1/"
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+
+toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_params = {
     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
     "geocode": toponym_to_find,
-    "format": "json"}
+    "format": "json"
+}
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
 
-if not response:
-    # обработка ошибочной ситуации
+response_geocoder = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response_geocoder:
     pass
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"][
+json_response_geocoder = response_geocoder.json()
+
+toponym = json_response_geocoder["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
-print(json_response["response"])
-# Координаты центра топонима:
+toponym_coodrinates_pos = toponym["Point"]["pos"]
 
-toponym_coodrinates_1 = toponym["boundedBy"]["Envelope"]["lowerCorner"]
-toponym_coodrinates_2 = toponym["boundedBy"]["Envelope"]["upperCorner"]
 
-toponym_longitude, toponym_lattitude, delta_x, delta_y = object_pos(toponym_coodrinates_1, toponym_coodrinates_2)
-# Собираем параметры для запроса к StaticMapsAPI:
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": ",".join(toponym_coodrinates_pos.split()),
+    "type": "biz"
+}
+
+response_search = requests.get(search_api_server, params=search_params)
+
+if not response_search:
+    pass
+
+json_response_search = response_search.json()
+
+org_point = ""
+organization = json_response_search["features"][0]
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+point = organization["geometry"]["coordinates"]
+org_point += "{0},{1}~".format(point[0], point[1])
+org_point = org_point[:-1]
+delta = "0.03"
+point = str(point[0]) + " " + str(point[1])
+toponym_longitude, toponym_lattitude, delta_x, delta_y = object_pos(point, toponym_coodrinates_pos)
 
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": ",".join([delta_x, delta_y]),
     "l": "map",
-    "pt": ",".join([toponym_longitude, toponym_lattitude]) + ",pm2am"
+    "pt": org_point + ",pm2bl~" + ",".join(toponym_coodrinates_pos.split()) + ",pm2al",
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
 
-Image.open(BytesIO(
-    response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+Image.open(BytesIO(response.content)).show()
--- pos.py
+++ pos.py
@@ -3,9 +3,12 @@ def object_pos(toponym_coodrinates_1, toponym_coodrinates_2):
     x_2, y_2 = toponym_coodrinates_2.split(" ")
     x_1, y_1 = float(x_1), float(y_1)
     x_2, y_2 = float(x_2), float(y_2)
-
+    if x_2 < x_1:
+        x_2, x_1 = x_1, x_2
+    if y_2 < y_1:
+        y_2, y_1 = y_1, y_2
     toponym_longitude, toponym_lattitude = str(((x_2 - x_1) / 2) + x_1), str(((y_2 - y_1) / 2) + y_1)
 
-    delta_x = str(float(toponym_longitude) - x_1)
-    delta_y = str(float(toponym_lattitude) - y_1)
+    delta_x = str(float(toponym_longitude) - x_1 * 0.999777)
+    delta_y = str(float(toponym_lattitude) - y_1 * 0.999777)
     return toponym_longitude, toponym_lattitude, delta_x, delta_y
